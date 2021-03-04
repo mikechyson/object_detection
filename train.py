859e212e6ef707296a2bd16f29e51729669a62b1
Mike Chyson <mile.chyson@gmail.com>
2021-03-05 00:05:31 +0800

    AnchorBoxes.py

---
+++ train.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+@project: object_detection
+@file: train
+@author: mike
+@time: 2021/3/4
+ 
+@function:
+"""
---
+++ utils/bounding_box.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+@project: object_detection
+@file: bounding_box
+@author: mike
+@time: 2021/3/4
+ 
+@function:
+"""
+
+
+def convert_coordinates(tensor, start_index, conversion, border_pixels='half'):
+    pass
